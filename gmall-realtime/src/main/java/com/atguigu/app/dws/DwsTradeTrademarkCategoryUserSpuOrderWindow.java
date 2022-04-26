d80448ea431eaf0a2a73d159b3c3065d411063c9
whalex <whalex@atguigu.com>
2022-04-26 17:04:56 +0800

    day12(2)

--- gmall-realtime/src/main/java/com/atguigu/app/dws/DwsTradeTrademarkCategoryUserSpuOrderWindow.java
+++ gmall-realtime/src/main/java/com/atguigu/app/dws/DwsTradeTrademarkCategoryUserSpuOrderWindow.java
@@ -1,17 +1,18 @@
 package com.atguigu.app.dws;
 
 import com.alibaba.fastjson.JSONObject;
+import com.atguigu.app.func.DimAsyncFunction;
 import com.atguigu.app.func.OrderDetailFilterFunction;
 import com.atguigu.bean.TradeTrademarkCategoryUserSpuOrderBean;
-import com.atguigu.utils.DimUtil;
-import org.apache.flink.api.common.functions.RichMapFunction;
-import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.AsyncDataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
+import java.util.concurrent.TimeUnit;
+
 public class DwsTradeTrademarkCategoryUserSpuOrderWindow {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         //TODO 1.获取执行环境
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
@@ -47,26 +48,137 @@ public class DwsTradeTrademarkCategoryUserSpuOrderWindow {
         );
 
         //TODO 4.关联维表
-        skuUserOrderDS.map(new RichMapFunction<TradeTrademarkCategoryUserSpuOrderBean, TradeTrademarkCategoryUserSpuOrderBean>() {
+//        skuUserOrderDS.map(new RichMapFunction<TradeTrademarkCategoryUserSpuOrderBean, TradeTrademarkCategoryUserSpuOrderBean>() {
+//            @Override
+//            public void open(Configuration parameters) throws Exception {
+//                //创建连接
+//            }
+//            @Override
+//            public TradeTrademarkCategoryUserSpuOrderBean map(TradeTrademarkCategoryUserSpuOrderBean value) throws Exception {
+//                //查询SKU表
+//                //DimUtil.getDimInfo(conn, "", value.getSkuId());
+//                //查询SPU表
+//                //... ...
+//                return value;
+//            }
+//        });
+        skuUserOrderDS.print("skuUserOrderDS>>>>");
 
-            @Override
-            public void open(Configuration parameters) throws Exception {
-                //创建连接
-            }
+        //4.1 关联SKU
+        SingleOutputStreamOperator<TradeTrademarkCategoryUserSpuOrderBean> withSkuDS = AsyncDataStream.unorderedWait(
+                skuUserOrderDS,
+                new DimAsyncFunction<TradeTrademarkCategoryUserSpuOrderBean>("DIM_SKU_INFO") {
+                    @Override
+                    public String getKey(TradeTrademarkCategoryUserSpuOrderBean input) {
+                        return input.getSkuId();
+                    }
 
-            @Override
-            public TradeTrademarkCategoryUserSpuOrderBean map(TradeTrademarkCategoryUserSpuOrderBean value) throws Exception {
+                    @Override
+                    public void join(TradeTrademarkCategoryUserSpuOrderBean input, JSONObject dimInfo) {
+                        if (dimInfo != null) {
+                            System.out.println("CATEGORY3_ID:" + dimInfo.getString("CATEGORY3_ID"));
+                            input.setSpuId(dimInfo.getString("SPU_ID"));
+                            input.setTrademarkId(dimInfo.getString("TM_ID"));
+                            input.setCategory3Id(dimInfo.getString("CATEGORY3_ID"));
+                        }
+                    }
+                },
+                60, TimeUnit.SECONDS);
 
-                //查询SKU表
-                //DimUtil.getDimInfo(conn, "", value.getSkuId());
+        //4.2 关联SPU
+        SingleOutputStreamOperator<TradeTrademarkCategoryUserSpuOrderBean> withSpuDS = AsyncDataStream.unorderedWait(
+                withSkuDS,
+                new DimAsyncFunction<TradeTrademarkCategoryUserSpuOrderBean>("DIM_SPU_INFO") {
+                    @Override
+                    public String getKey(TradeTrademarkCategoryUserSpuOrderBean input) {
+                        return input.getSpuId();
+                    }
+                    @Override
+                    public void join(TradeTrademarkCategoryUserSpuOrderBean input, JSONObject dimInfo) {
+                        if (dimInfo != null) {
+                            input.setSpuName(dimInfo.getString("SPU_NAME"));
+                        }
+                    }
+                },
+                60, TimeUnit.SECONDS);
 
-                //查询SPU表
+        //4.3 关联TM
+        SingleOutputStreamOperator<TradeTrademarkCategoryUserSpuOrderBean> withTmDS = AsyncDataStream.unorderedWait(
+                withSpuDS,
+                new DimAsyncFunction<TradeTrademarkCategoryUserSpuOrderBean>("DIM_BASE_TRADEMARK") {
+                    @Override
+                    public String getKey(TradeTrademarkCategoryUserSpuOrderBean input) {
+                        return input.getTrademarkId();
+                    }
 
-                //... ...
+                    @Override
+                    public void join(TradeTrademarkCategoryUserSpuOrderBean input, JSONObject dimInfo) {
+                        if (dimInfo != null) {
+                            input.setTrademarkName(dimInfo.getString("TM_NAME"));
+                        }
+                    }
+                },
+                60, TimeUnit.SECONDS);
 
-                return value;
-            }
-        });
+        //4.4 关联Category3
+        SingleOutputStreamOperator<TradeTrademarkCategoryUserSpuOrderBean> withCategory3DS = AsyncDataStream.unorderedWait(
+                withTmDS,
+                new DimAsyncFunction<TradeTrademarkCategoryUserSpuOrderBean>("DIM_BASE_CATEGORY3") {
+                    @Override
+                    public String getKey(TradeTrademarkCategoryUserSpuOrderBean input) {
+                        System.out.println("input.getCategory3Id():" + input.getCategory3Id());
+                        return input.getCategory3Id();
+                    }
+
+                    @Override
+                    public void join(TradeTrademarkCategoryUserSpuOrderBean input, JSONObject dimInfo) {
+                        if (dimInfo != null) {
+                            input.setCategory3Name(dimInfo.getString("NAME"));
+                            input.setCategory2Id(dimInfo.getString("CATEGORY2_ID"));
+                        }
+                    }
+                },
+                60, TimeUnit.SECONDS);
+
+        //4.5 关联Category2
+        SingleOutputStreamOperator<TradeTrademarkCategoryUserSpuOrderBean> withCategory2DS = AsyncDataStream.unorderedWait(
+                withCategory3DS,
+                new DimAsyncFunction<TradeTrademarkCategoryUserSpuOrderBean>("DIM_BASE_CATEGORY2") {
+                    @Override
+                    public String getKey(TradeTrademarkCategoryUserSpuOrderBean input) {
+                        return input.getCategory2Id();
+                    }
+
+                    @Override
+                    public void join(TradeTrademarkCategoryUserSpuOrderBean input, JSONObject dimInfo) {
+                        if (dimInfo != null) {
+                            input.setCategory2Name(dimInfo.getString("NAME"));
+                            input.setCategory1Id(dimInfo.getString("CATEGORY1_ID"));
+                        }
+                    }
+                },
+                60, TimeUnit.SECONDS);
+
+        //4.6 关联Category1
+        SingleOutputStreamOperator<TradeTrademarkCategoryUserSpuOrderBean> withCategory1DS = AsyncDataStream.unorderedWait(
+                withCategory2DS,
+                new DimAsyncFunction<TradeTrademarkCategoryUserSpuOrderBean>("DIM_BASE_CATEGORY1") {
+                    @Override
+                    public String getKey(TradeTrademarkCategoryUserSpuOrderBean input) {
+                        return input.getCategory1Id();
+                    }
+
+                    @Override
+                    public void join(TradeTrademarkCategoryUserSpuOrderBean input, JSONObject dimInfo) {
+                        if (dimInfo != null) {
+                            input.setCategory1Name(dimInfo.getString("NAME"));
+                        }
+                    }
+                },
+                60, TimeUnit.SECONDS);
+
+        //打印测试
+        withCategory1DS.print("withCategory1DS>>>>>>>>");
 
         //TODO 5.提取时间戳生成WaterMark
 
@@ -75,6 +187,7 @@ public class DwsTradeTrademarkCategoryUserSpuOrderWindow {
         //TODO 7.将数据写出到ClickHouse
 
         //TODO 8.启动任务
+        env.execute("DwsTradeTrademarkCategoryUserSpuOrderWindow");
 
     }
 
--- gmall-realtime/src/main/java/com/atguigu/utils/DimUtil.java
+++ gmall-realtime/src/main/java/com/atguigu/utils/DimUtil.java
@@ -28,6 +28,7 @@ public class DimUtil {
 
         //拼接SQL
         String querySql = "select * from " + GmallConfig.HBASE_SCHEMA + "." + tableName + " where id ='" + id + "'";
+        System.out.println("查询SQL为：" + querySql);
 
         //查询Phoenix
         List<JSONObject> list = JdbcUtil.queryList(connection, querySql, JSONObject.class, false);
